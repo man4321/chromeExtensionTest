d11bf786132b06fa5a3e70a63cff3e96ca5db30d
Ashok Seervi <52868446+ashokseervi14@users.noreply.github.com>
2020-05-11 15:45:35 +0530

    attributes added

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('myExt');
     checkPageButton.addEventListener('click',function(){
-        window.open("www.google.com"); },false);
+        window.open("../www.google.com"); },false);
     
     },false);
                           
